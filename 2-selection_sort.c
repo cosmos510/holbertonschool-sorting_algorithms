69797ba4605a2554a73a99004c6d05cf51406254
cosmos510 <maximemartin510@gmail.com>
2024-04-11 19:13:37 +0000

    fixing betty style

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,26 +8,24 @@
 */
 void selection_sort(int *array, size_t size)
 {
-	size_t i,j;
+	size_t i, j;
 	size_t minIndex;
 	int temp;
 
-	for ( i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-    	minIndex = i;
-    	for ( j = i + 1; j < size; j++)
-    	{
-        	if (array[j] < array[minIndex])
-        	{
-        	minIndex = j;
-        	}
-    	}
+		minIndex = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[minIndex])
+				minIndex = j;
+		}
 		if (minIndex != i)
 		{
-    		temp = array[i];
-    		array[i] = array[minIndex];
-    		array[minIndex] = temp;
-    		print_array(array, size);
+			temp = array[i];
+			array[i] = array[minIndex];
+			array[minIndex] = temp;
+			print_array(array, size);
 		}
 	}
 }
